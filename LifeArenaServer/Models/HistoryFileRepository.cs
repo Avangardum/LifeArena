82484b383416b2a3d4a664aca6b0543b96d78b18
Avangardum <yurykuz65@gmail.com>
2023-04-13 18:21:34 +0300

    Making history file repository

---
+++ LifeArenaServer/Models/HistoryFileRepository.cs
@@ -0,0 +1,44 @@
+﻿using Avangardum.LifeArena.Server.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Avangardum.LifeArena.Server.Models;
+
+public class HistoryFileRepository : IHistoryRepository
+{
+    private const string HistoryDirectoryPath = "History";
+    private const string HistoryFileExtension = ".json";
+    private const string LivingCellsPropertyName = "livingCells";
+    private const string GenerationPropertyName = "generation";
+
+    private ILivingCellsArrayPreserializer _livingCellsArrayPreserializer;
+
+    public HistoryFileRepository(ILivingCellsArrayPreserializer livingCellsArrayPreserializer)
+    {
+        _livingCellsArrayPreserializer = livingCellsArrayPreserializer;
+        
+        if (!Directory.Exists(HistoryDirectoryPath))
+        {
+            Directory.CreateDirectory(HistoryDirectoryPath);
+        }
+    }
+    
+    public int LastSnapshotGeneration { get; }
+    
+    public void SaveSnapshot(GameSnapshot snapshot)
+    {
+        var preserializedLivingCells = _livingCellsArrayPreserializer.Preserialize(snapshot.LivingCells);
+        var json = new JObject
+        {
+            new JProperty(LivingCellsPropertyName, preserializedLivingCells),
+            new JProperty(GenerationPropertyName, snapshot.Generation)
+        };
+        var path = Path.Combine(HistoryDirectoryPath, snapshot.Generation.ToString(), HistoryFileExtension);
+        File.WriteAllText(path, json.ToString());
+    }
+
+    public GameSnapshot LoadSnapshot(int generation)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LifeArenaServerUnitTests/HistoryFileRepositoryTests.cs
@@ -0,0 +1,48 @@
+﻿using Avangardum.LifeArena.Server.Helpers;
+using Avangardum.LifeArena.Server.Interfaces;
+using Avangardum.LifeArena.Server.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Avangardum.LifeArena.Server.UnitTests;
+
+[TestFixture]
+public class HistoryFileRepositoryTests
+{
+    private const string HistoryDirectoryPath = "History";
+    private const string HistoryFileExtension = ".json";
+
+    #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+    private ILivingCellsArrayPreserializer _livingCellsArrayPreserializer;
+    private IHistoryRepository _repository;
+    #pragma warning restore CS8618
+
+    [SetUp]
+    public void SetUp()
+    {
+        _livingCellsArrayPreserializer = new LivingCellsArrayPreserializer();
+        _repository = new HistoryFileRepository(_livingCellsArrayPreserializer);
+        
+        if (Directory.Exists(HistoryDirectoryPath))
+        {
+            Directory.Delete(HistoryDirectoryPath);
+        }
+    }
+    
+    [Test]
+    public void SavesSnapshotToFile()
+    {
+        var livingCells = new bool[2, 2];
+        livingCells[1, 1] = true;
+        var generation = 7;
+        var snapshot = new GameSnapshot(livingCells, generation);
+        _repository.SaveSnapshot(snapshot);
+        var filePath = Path.Combine(HistoryDirectoryPath, generation.ToString(), HistoryFileExtension);
+        var rawJson = File.ReadAllText(filePath).Replace(" ", "").Replace("\n", "");
+        var json = new JObject(rawJson);
+        var fileLivingCells = json["livingCells"]?.ToObject<List<string>>();
+        var fileGeneration = json["generation"]?.ToObject<int>();
+        var expectedFileLivingCells = new List<string> { ".0", ".." };
+        Assert.That(fileLivingCells, Is.EqualTo(expectedFileLivingCells));
+        Assert.That(fileGeneration, Is.EqualTo(generation));
+    }
+}
