f56c9e3b2e257b69fb08414d541503d1f591682a
Avangardum <yurykuz65@gmail.com>
2023-05-05 14:53:24 +0300

    Updated client, added Github links, added README

---
+++ LifeArenaServer/Pages/GithubLinks.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Avangardum.LifeArena.Server.Pages;
+
+public class GithubLinks : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
