2eb285b098f11a2e8552bbcf397046f7583330e7
Avangardum <yurykuz65@gmail.com>
2023-10-02 11:39:56 +0300

    Made dragging the field

---
+++ LifeArenaBlazorClient/wwwroot/js/LifeArenaFieldDragHelper.js
@@ -0,0 +1,22 @@
+﻿function makeCssClassDraggable(draggableCssClass) {
+    if (!draggableCssClass.startsWith("."))
+        draggableCssClass = "." + draggableCssClass
+    
+    interact(draggableCssClass).draggable({
+        inertia: true,
+        listeners: {
+            move(event) {
+                let target = event.target
+                let x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+                let y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+                
+                target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+                
+                target.setAttribute('data-x', x)
+                target.setAttribute('data-y', y)
+            }
+        }
+    })
+    
+    interact(draggableCssClass).styleCursor(false)
+}
