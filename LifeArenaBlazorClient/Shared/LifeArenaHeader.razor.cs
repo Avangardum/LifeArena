a367c5317418c95c8912d15a7eba917a61111cea
Avangardum <yurykuz65@gmail.com>
2023-10-05 15:49:04 +0300

    Refactored LifeArenHeader by splitting it into section child components.

--- LifeArenaBlazorClient/Shared/LifeArenaHeader.razor.cs
+++ LifeArenaBlazorClient/Shared/LifeArenaHeader.razor.cs
@@ -4,62 +4,16 @@ namespace LifeArenaBlazorClient.Shared;
 
 public partial class LifeArenaHeader
 {
-    /// <summary>
-    /// If the timer tries to increase by less than this amount (due to network latency), ignore it to avoid
-    /// jittering. If the timer tries to increase by more or equal than this amount (due to generation change),
-    /// allow it. 
-    /// </summary>
-    private static readonly TimeSpan MinTimeToAllowIncreasingTimer = TimeSpan.FromSeconds(1);
+    private TimeUntilNextGenerationHeaderSection _timeUntilNextGenerationHeaderSection = null!;
     
-    private const string White = "white";
-    private const string Black = "black";
-    
-    private Stopwatch _stopwatch = new();
-    private TimeSpan _timeUntilNextGeneration;
-
     public TimeSpan NextGenerationInterval { get; set; }
     public int Generation { get; set; }
     public int CellsLeft { get; set; }
 
     public TimeSpan TimeUntilNextGeneration
     {
-        get => _timeUntilNextGeneration;
-        set
-        {
-            var deltaTime = value - _timeUntilNextGeneration;
-            if (TimeSpan.Zero < deltaTime && deltaTime < MinTimeToAllowIncreasingTimer) return;
-            _timeUntilNextGeneration = value;
-            if (_timeUntilNextGeneration < TimeSpan.Zero) _timeUntilNextGeneration = TimeSpan.Zero;
-        }
-    }
-
-    private string TimeUntilNextGenerationClockStyle
-    {
-        get
-        {
-            var firstColor = Generation % 2 == 0 ? White : Black;
-            var secondColor = Generation % 2 == 0 ? Black : White;
-            var firstColorPercentage = TimeUntilNextGeneration / NextGenerationInterval;
-            return $"background: conic-gradient({firstColor} {1 - firstColorPercentage:P}, {secondColor} 0)";
-        }
+        set => _timeUntilNextGenerationHeaderSection.TimeUntilNextGeneration = value;
     }
 
     public void InvokeStateHasChanged() => StateHasChanged();
-
-    protected override async Task OnInitializedAsync()
-    {
-        await TimerUpdateLoop();
-    }
-
-    private async Task TimerUpdateLoop()
-    {
-        while (true)
-        {
-            await Task.Delay(TimeSpan.FromMilliseconds(20));
-            TimeUntilNextGeneration -= _stopwatch.Elapsed;
-            _stopwatch.Restart();
-            StateHasChanged();
-        }
-        // ReSharper disable once FunctionNeverReturns
-    }
 }
---
+++ LifeArenaBlazorClient/Shared/TimeUntilNextGenerationHeaderSection.razor.cs
@@ -0,0 +1,71 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Components;
+
+namespace LifeArenaBlazorClient.Shared;
+
+public partial class TimeUntilNextGenerationHeaderSection
+{
+    /// <summary>
+    /// If the timer tries to increase by less than this amount (due to network latency), ignore it to avoid
+    /// jittering. If the timer tries to increase by more or equal than this amount (due to generation change),
+    /// allow it. 
+    /// </summary>
+    private static readonly TimeSpan MinTimeToAllowIncreasingTimer = TimeSpan.FromSeconds(1);
+    
+    private const string White = "white";
+    private const string Black = "black";
+    
+    private Stopwatch _stopwatch = new();
+    private TimeSpan _timeUntilNextGeneration;
+    
+    [Parameter]
+    public required int Generation { get; set; }
+    
+    [Parameter]
+    public required TimeSpan NextGenerationInterval { get; set; }
+    
+    public TimeSpan TimeUntilNextGeneration
+    {
+        get => _timeUntilNextGeneration;
+        set
+        {
+            var deltaTime = value - _timeUntilNextGeneration;
+            if (TimeSpan.Zero < deltaTime && deltaTime < MinTimeToAllowIncreasingTimer) return;
+            _timeUntilNextGeneration = value;
+            if (_timeUntilNextGeneration < TimeSpan.Zero) _timeUntilNextGeneration = TimeSpan.Zero;
+        }
+    }
+
+    private string TimeUntilNextGenerationClockStyle
+    {
+        get
+        {
+            var firstColor = Generation % 2 == 0 ? White : Black;
+            var secondColor = Generation % 2 == 0 ? Black : White;
+            var firstColorPercentage = TimeUntilNextGeneration / NextGenerationInterval;
+            return $"background: conic-gradient({firstColor} {1 - firstColorPercentage:P}, {secondColor} 0)";
+        }
+    }
+    
+    protected override async Task OnInitializedAsync()
+    {
+        await TimerUpdateLoop();
+    }
+
+    private async Task TimerUpdateLoop()
+    {
+        while (true)
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(20));
+            TimeUntilNextGeneration -= _stopwatch.Elapsed;
+            _stopwatch.Restart();
+            StateHasChanged();
+        }
+        // ReSharper disable once FunctionNeverReturns
+    }
+
+    public TimeUntilNextGenerationHeaderSection()
+    {
+        Console.WriteLine("TimeUntilNextGenerationHeaderSection constructor");
+    }
+}
