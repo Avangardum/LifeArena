e3ec5fc8f3585a6574c652eaac1d73e5770aa7f3
Avangardum <yurykuz65@gmail.com>
2023-10-11 19:11:27 +0300

    Enabled AOT compilation

---
+++ LifeArenaBlazorClientHost/wwwroot/js/JsHelper.js
@@ -0,0 +1,23 @@
+﻿function makeLifeArenaFieldDraggable(targetClass, lifeArenaBodyComponent) {
+    if (!targetClass.startsWith("."))
+        targetClass = "." + targetClass
+    
+    interact(targetClass).draggable({
+        inertia: true,
+        listeners: {
+            move(event) {
+                let target = event.target
+                let x = (parseFloat(target.getAttribute('field-translate-x')) || 0) + event.dx
+                let y = (parseFloat(target.getAttribute('field-translate-y')) || 0) + event.dy
+                
+                lifeArenaBodyComponent.invokeMethod("SetFieldTranslate", x, y)
+            }
+        }
+    })
+    
+    interact(targetClass).styleCursor(false)
+}
+
+function getBoundingClientRect(elementId) {
+    return document.getElementById(elementId).getBoundingClientRect()
+}
